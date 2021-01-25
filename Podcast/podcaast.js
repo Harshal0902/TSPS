ec934f7e463c3e23ec45c961dfd62eb781b59189
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2021-01-25 22:03:24 +0530

    Add files via upload

---
+++ Podcast/podcaast.js
@@ -0,0 +1,8 @@
+  
+// Responsive nav bar item
+$(document).ready(function () {
+    $('.btn').click(function () {
+        $('.items').toggleClass("show");
+        $('ul li').toggleClass("hide");
+    });
+});
