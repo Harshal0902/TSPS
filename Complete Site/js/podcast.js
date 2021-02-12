af3011e5f46875fc3ee0086c5b7abc1768316354
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2021-02-12 15:50:02 +0530

    Update podcast.js

--- Complete Site/js/podcast.js
+++ Complete Site/js/podcast.js
@@ -7,6 +7,30 @@ $(document).ready(function () {
 });
 
 
+
+$(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0:{
+        items:1,
+        nav: false
+      },
+      600:{
+        items:2,
+        nav: false
+      },
+      1000:{
+        items:3,
+        nav: false
+      }
+    }
+  });
+  
+
 // Scroll to top button
 const backToTopButton = document.querySelector("#back-to-top-btn");
 
